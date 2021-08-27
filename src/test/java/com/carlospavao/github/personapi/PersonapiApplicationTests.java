a08334a205741d6c54cfa2cf067b8644bcc0001b
Carlos Pavão <carlos.henrique93@msn.com>
2021-08-27 12:01:48 -0300

    primeiro commit

---
+++ src/test/java/com/carlospavao/github/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlospavao.github.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
